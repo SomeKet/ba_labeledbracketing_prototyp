a08fc93364a9802411aa396a93983c4fa3bdb711
Mario <mario-mai@gmx.net>
2025-05-28 13:21:57 +0200

    Erstellung Kategorie; Anzeige Kategorie; Prüfe ob Dupletten Kategorie

--- script.js
+++ script.js
@@ -11,3 +11,59 @@ tinymce.init({
     toolbar: 'abc | code | save | highlight | textmarkierung | tagger | undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
         
 }); 
+
+let categories = [];
+
+document.getElementById('categoryForm').addEventListener('submit', (e) => {
+    e.preventDefault(); //verhinder redirect
+
+    const data = new FormData(e.target);
+    const name = data.get("name");
+    const label = data.get("label");
+    const color = data.get("color");
+    const extra = data.get("extra");
+
+    if(!checkCategoryDuplette(name, label, color)){
+        return;    
+    }else{
+        console.log(name, label, color, extra);
+        createCategory(name, label, color, extra);
+        document.getElementById('categoryForm').reset();
+    }
+
+
+})
+
+function createCategory(name, label, color, extra){
+    categories.push({name, label, color, extra});
+
+    let btn = document.createElement("button");
+    btn.className=`${label}`;
+    let textNode = document.createTextNode(name);
+    btn.appendChild(textNode);
+    btn.style.background = color;
+    document.getElementById("lecButtons").appendChild(btn);
+}
+
+function checkCategoryDuplette(name, label, color){
+    const exists = categories.some(category => {
+        if(category.name.toLowerCase() === name.toLowerCase()){
+            alert("Bezeichnung existiert bereits");
+            return true;
+        }
+        else if(category.label.toLowerCase() === label.toLowerCase()){
+            alert("Label existiert bereits");
+            return true;
+        }
+        else if(category.color.toLowerCase() === color.toLowerCase()){
+            alert("Farbe existiert bereits");
+            return true;
+        }
+        return false;
+    });
+    
+    if(!exists){
+        alert("Kategorie wurde gespeichert");
+    }
+    return !exists;
+}
